928d65dd2bffb416e3b81335345ed66901ad6d75
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-01-24 17:31:16 +0000

    Update FUNguild.sh

--- FUNguild.sh
+++ FUNguild.sh
@@ -19,10 +19,20 @@ assign_taxonomy.py \
 
 # FUNguild requires a specific input file which is a combination of the qiime txt and log output files (and OTU table)
 grep -E "^H" V2_FUN.otus_tax_assignments.log|awk -F"[\t_]" '{split($10,a,"\.");print $9,$4"%",a[1],a[2]"_"$11}' OFS="\t" > log.txt
+cat V2_FUN.otus_tax_assignments.txt|mod_taxa_qiime.pl > tax.txt
 
 # then from R
 library(dplyr)
 
-tax <- read.table("V2_FUN.otus_tax_assignments.txt",sep="\t",header=F)
+tax <- read.table("tax.txt",sep=",",header=F)
 log <- read.table("log.txt",sep="\t",header=F)
-countData <- read.table("../FUN.otus_table.txt",sep="\t",header=T,row.names=1, comment.char = "")
+countData <- read.table("../FUN.otus_table.txt",sep="\t",header=T, comment.char = "")
+
+log$perc <- as.numeric(sub("%","",log$V2))
+test <- log  %>% group_by(V1) %>% summarise(perc=max(perc))
+test2 <- left_join(test,log)
+test2<-test2[!duplicated(test2$V1),]
+log_tax <- left_join(test2,tax,by=c("V1"="V1"))
+counts_log_tax <- left_join(countData,log_tax,by=c("X.OTU.ID","V1))
+counts_log_tax$taxonomy <- paste(counts_log_tax$V2.x,counts_log_tax$V8,counts_log_tax$V3.x,counts_log_tax$V4.x,"reps",paste(counts_log_tax$V2.y,counts_log_tax$V3.y,counts_log_tax$V4.y,counts_log_tax$V5,counts_log_tax$V6,counts_log_tax$V7,counts_log_tax$V8,sep=";") ,sep="|")
+
