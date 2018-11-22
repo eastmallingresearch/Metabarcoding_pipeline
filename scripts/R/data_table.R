76825397ce3ed2222c708ae6017716ee98d3a651
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-11-22 12:50:42 +0000

    Update data_table.R

--- scripts/R/data_table.R
+++ scripts/R/data_table.R
@@ -1,11 +1,13 @@
 # functions for use with data table
 
+#sets NA to 0 by reference
 unsetNA = function(DT) {
   # or by number (slightly faster than by name) :
   for (j in seq_len(ncol(DT)))
     set(DT,which(is.na(DT[[j]])),j,0)
 }
 
+#sets 0 to NA by ref
 setNA = function(DT) {
   for (j in seq_len(ncol(DT)))
     set(DT,which(DT[[j]]==0),j,NA)
