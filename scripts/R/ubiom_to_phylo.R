7f4a5621a2be532bb186b1af93aa1ee6fed18508
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-12-11 12:10:23 +0000

    Update ubiom_to_phylo.R

--- scripts/R/ubiom_to_phylo.R
+++ scripts/R/ubiom_to_phylo.R
@@ -4,7 +4,7 @@ ubiom_to_phylo <- function(
 	suppressPackageStartupMessages(require(phyloseq))
 	phyloseq(
 		otu_table(obj[[1]],taxa_are_rows=T),
-	 	tax_table(as.matrix(obj[[2]])),
-	 	sample_data(obj[[3]])
+	 	tax_table(as.matrix(obj[[3]])),
+	 	sample_data(obj[[2]])
 	 )
 }
