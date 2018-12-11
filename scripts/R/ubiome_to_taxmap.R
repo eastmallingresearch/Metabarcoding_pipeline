59b7f87ae7d4683175917580f3909e22098352eb
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-12-11 16:39:33 +0000

    Update ubiome_to_taxmap.R

--- scripts/R/ubiome_to_taxmap.R
+++ scripts/R/ubiome_to_taxmap.R
@@ -7,9 +7,18 @@ ubiome_to_taxmap <- function(
 	sample_data  <- as.tibble(obj[[2]],rownames="sample_id")
 	tax_data  <- as.tibble(obj[[3]],rownames="otu_id")
 	#phy_data  <- if(!is.null(obj[[4]])){obj[[4]]} else{NULL}
-
-	parsed_tax <- lapply(unlist(tax_data[,2:8]), trimws)
+	parsed_tax <- lapply(seq_len(nrow(tax_data)), function(i) {
+		class_source <- unlist(lapply(tax_data[i, 2:8], as.character))
+		unlist(class_source)
+	})
+	parsed_tax <- lapply(parsed_tax, trimws)
 	output <- taxmap(.list = parsed_tax, named_by_rank = F)
+	# set the taxon_id to the rank (rank is the lowest defined rank with a given confidence)
+	t1 <- output$taxon_names()
+	t2 <- sub("\\(.*","",tax_data$rank) 
+	t3 <- sapply(t2,function(x) names(t1[t1==x])[1])
+#	otu_table$taxon_id <- t3
+	tax_data$taxon_id <- t3
 	output$data <- list(otu_table=otu_table,tax_data=tax_data,sample_data=sample_data)
 	output
 }
