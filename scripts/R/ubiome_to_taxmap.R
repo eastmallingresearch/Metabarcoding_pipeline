6e2bfaee581bb0a53c060b90c2f5959558899ebf
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-12-12 17:03:11 +0000

    Update ubiome_to_taxmap.R

--- scripts/R/ubiome_to_taxmap.R
+++ scripts/R/ubiome_to_taxmap.R
@@ -6,14 +6,24 @@ ubiome_to_taxmap <- function(
 	otu_table <- as.tibble(obj[[1]],rownames="otu_id")
 	#sample_data  <- as.tibble(obj[[2]],rownames="sample_id")
 	tax_data  <- as.tibble(obj[[3]],rownames="otu_id")
-	#phy_data  <- if(!is.null(obj[[4]])){obj[[4]]} else{NULL}
-	parsed_tax <- lapply(seq_len(nrow(tax_data)), function(i) {
-		class_source <- unlist(lapply(tax_data[i, 2:8], as.character))
-		unlist(class_source)
+	parsed_tax <- apply(td[1:3,2:8],1, function(x) {
+		x <- x[!is.na(x)]
+		xx<-hierarchy(x)
+		xx <- lapply(seq_along(xx),function(i) {
+			xx$taxa[[i]]$rank$name <- names(x)[i]
+			xx
+		})
+		xx
 	})
-	
+#		xx$taxa[[1]]$rank$name <- "kingdom"
+#		xx$taxa[[2]]$rank$name <- "phylum"
+#		xx$taxa[[3]]$rank$name <- "class"
+#		xx$taxa[[4]]$rank$name <- "order"
+#		xx$taxa[[5]]$rank$name <- "family"
+#		xx$taxa[[6]]$rank$name <- "genus"
+#		xx$taxa[[7]]$rank$name <- "species"	
 	#	parsed_tax <- lapply(parsed_tax, trimws) # removes white space (should't be any)
-	output <- taxmap(.list = parsed_tax, named_by_rank = F)
+	output <- taxmap(.list = parsed_tax, named_by_rank = T)
 	# set the taxon_id to the rank (rank is the lowest defined rank with a given confidence)
 	t1 <- output$taxon_names()
 	t2 <- sub("\\(.*","",tax_data$rank) 
