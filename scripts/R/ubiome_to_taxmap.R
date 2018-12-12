ae57b38af4eff24c8f2153b5274f99ed139c6170
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-12-12 14:13:33 +0000

    Update ubiome_to_taxmap.R

--- scripts/R/ubiome_to_taxmap.R
+++ scripts/R/ubiome_to_taxmap.R
@@ -4,22 +4,21 @@ ubiome_to_taxmap <- function(
 	suppressPackageStartupMessages(require(metacoder))
 	# tibblise data
 	otu_table <- as.tibble(obj[[1]],rownames="otu_id")
-	sample_data  <- as.tibble(obj[[2]],rownames="sample_id")
+	#sample_data  <- as.tibble(obj[[2]],rownames="sample_id")
 	tax_data  <- as.tibble(obj[[3]],rownames="otu_id")
 	#phy_data  <- if(!is.null(obj[[4]])){obj[[4]]} else{NULL}
 	parsed_tax <- lapply(seq_len(nrow(tax_data)), function(i) {
 		class_source <- unlist(lapply(tax_data[i, 2:8], as.character))
 		unlist(class_source)
 	})
-	parsed_tax <- lapply(parsed_tax, trimws)
+	
+	#	parsed_tax <- lapply(parsed_tax, trimws) # removes white space (should't be any)
 	output <- taxmap(.list = parsed_tax, named_by_rank = F)
 	# set the taxon_id to the rank (rank is the lowest defined rank with a given confidence)
 	t1 <- output$taxon_names()
 	t2 <- sub("\\(.*","",tax_data$rank) 
 	t3 <- sapply(t2,function(x) names(t1[t1==x])[1])
-#	otu_table$taxon_id <- t3
-	tax_data$taxon_id <- t3
-	output$data <- list(otu_table=otu_table,tax_data=tax_data,sample_data=sample_data)
-	output
+	output$data$otu_table<-as.tibble(cbind(taxon_id=t3,otu_table))
+	output	
 }
 
